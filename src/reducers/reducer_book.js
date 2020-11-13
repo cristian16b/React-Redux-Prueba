6f66d281f962b3391a43fc2f8b152ff5928fad26
cristian16b <cristian.budzicz@gmail.com>
2020-11-13 10:35:06 -0300

    Creo un reducer para books

---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,9 @@
+export default  function () {
+   return(
+       [
+           {title : "java para iniciantes"},
+           {title : "javascript avanzado"},
+           {title : "c++ universitarios"},
+       ]
+   ) 
+}
